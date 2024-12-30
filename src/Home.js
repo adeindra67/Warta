8dc27393ab9860d640f205a5501200452b80aa1c
17223021 <jantama@duck.com>
2024-12-30 09:25:46 +0000

    update Homepage dan menambahkan assets

--- src/Home.js
+++ src/Home.js
@@ -1,115 +1,107 @@
-import React, {useEffect, useState} from 'react';
-import { GluestackUIStyledProvider, Button, ButtonText } from '@gluestack-ui/themed';
+import React, { useEffect, useState } from 'react';
+import { GluestackUIStyledProvider, Button, ButtonText, View, ScrollView, Input } from '@gluestack-ui/themed';
 import { config } from '@gluestack-ui/config';
-import { GluestackUIProvider, Alert, FlatList, AlertText, Card, Image, Text, Heading, Link, HStack, LinkText, } from '@gluestack-ui/themed';
-import {ActivityIndicator} from 'react-native';
+import { GluestackUIProvider, Card, Image, Text, Heading, Link, HStack, LinkText, Tabs, Tab } from '@gluestack-ui/themed';
+import { ActivityIndicator, TouchableOpacity } from 'react-native';
 
+const categories = ['Kesehatan', 'Bisnis', 'Seni', 'Olahraga'];
 
-const Berita = () =>{
+const Home = ({ navigation }) => {
     const [isLoading, setLoading] = useState(true);
     const [data, setData] = useState([]);
+    const [category, setCategory] = useState('Semua'); 
+    const [searchTerm, setSearchTerm] = useState(''); // State for search term
 
-    const getMovies = async () => {
+    const getNews = async (category, searchTerm = '') => {
         try {
-        const response = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=899d3bc9552d47b68f7dc0785ef4023c');
-        const json = await response.json();
-        setData(json.articles);
+            let query = searchTerm ? searchTerm : category;
+            const response = await fetch(`https://newsapi.org/v2/everything?q=${query}&pageSize=5&apiKey=6d4b261c262e4a51a4408f2e839d5530`);
+            const json = await response.json();
+            setData(json.articles);
         } catch (error) {
-        console.error(error);
+            console.error(error);
         } finally {
-        setLoading(false);
+            setLoading(false);
         }
     };
 
     useEffect(() => {
-        getMovies();
-    }, []);
-    
+        getNews(category, searchTerm);
+    }, [category, searchTerm]);
+
+    const resetCategory = () => {
+      setCategory('Semua');
+      setSearchTerm('');
+    };
+
     return (
-        <>
-        {isLoading ? (
-            <ActivityIndicator />
-          ) : (
-            <FlatList
-                data={data}
-                renderItem={({ item }) => (
-                <Card p="$5" borderRadius="$lg" m="$3">
+        <GluestackUIProvider config={config}>
+            <View style={{ flex: 1 }}>
+                {/* Header with Logo and Search */}
+                <View style={{ padding: 10, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', borderBottomWidth: 1, borderBottomColor: '#ccc' }}>
                     <Image
-                    mb="$6"
-                    h={240}
-                    width="$full"
-                    borderRadius="$md"
-                    alt="image"
-                    source={{
-                    uri:'' + item.urlToImage+'',
-                    }}
+                        source={require('../assets/images/logo_warta.png')}
+                        alt="Warta Logo"
+                        style={{ width: 100, height: 50 }}
+                    />
+                    <Input 
+                        placeholder="Search News"
+                        value={searchTerm}
+                        onChangeText={setSearchTerm}
+                        style={{ flex: 1, marginLeft: 10, paddingHorizontal: 10 }}
+                    />
+                </View>
+
+                {/* Category Navigation */}
+                <View style={{ padding: 10, flexDirection: 'row', justifyContent: 'space-around' }}>
+                    {categories.map((cat) => (
+                        <Button key={cat} onPress={() => {
+                            setCategory(cat);
+                            setSearchTerm(''); // Clear search when selecting a category
+                        }} style={{ padding: 10, borderRadius: 20, backgroundColor: category === cat ? '#007AFF' : 'white' }}>
+                            <ButtonText style={{ color: category === cat ? 'white' : 'black' }}>{cat}</ButtonText>
+                        </Button>
+                    ))}
+                </View>
+
+                {isLoading ? (
+                    <ActivityIndicator />
+                ) : (
+                    <ScrollView>
+                        {data.map((item, index) => (
+                            <Card key={index} style={{ margin: 15, borderRadius: 10, shadowColor: '#000', shadowOffset: { width: 0, height: 2 }, shadowOpacity: 0.25, shadowRadius: 3.84, elevation: 5 }}>
+                                <Image
+                                    alt="Header Berita" 
+                                    source={{ uri: typeof item.urlToImage === 'string' ? item.urlToImage : 'https://via.placeholder.com/200' }}
+                                    style={{ width: '100%', height: 200, borderRadius: 10 }}
+                                />
+                                <Text style={{ padding: 10, fontSize: 12 }}>{item.publishedAt}</Text>
+                                <Heading style={{ padding: 10, fontSize: 16 }}>{item.title}</Heading>
+                                <Link onPress={() => navigation.navigate('Berita', { article: item })}>
+                                    <HStack alignItems="center">
+                                        <LinkText style={{ padding: 10, color: '#007AFF', textDecorationLine: 'none', fontWeight: 'bold' }}>Read More</LinkText>
+                                    </HStack>
+                                </Link>
+                            </Card>
+                        ))}
+                    </ScrollView>
+                )}
+            </View>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center', paddingVertical: 20, borderTopWidth: 1, borderTopColor: '#ccc' }}>
+                <TouchableOpacity onPress={resetCategory}> {/* Wrap the Image in TouchableOpacity */}
+                    <Image 
+                        source={require('../assets/images/home.png')} 
+                        style={{ width: 25, height: 25 }} 
+                    />
+                </TouchableOpacity>
+                <Text onPress={resetCategory} style={{ fontSize: 20, marginHorizontal: 10 }}>|</Text>
+                <Image 
+                    source={require('../assets/images/profile.png')} 
+                    style={{ width: 25, height: 25 }} 
                 />
-                <Text
-                    fontSize="$sm"
-                    fontStyle="normal"
-                    fontFamily="$heading"
-                    fontWeight="$normal"
-                    lineHeight="$sm"
-                    mb="$2"
-                    sx={{
-                    color: '$textLight700',
-                    _dark: {
-                        color: '$textDark200',
-                    },
-                    }}
-                >
-                    {item.publishedAt}
-                </Text>
-                <Heading size="md" fontFamily="$heading" mb="$4">
-                    {item.title}
-                </Heading>
-                <Link href={item.url} isExternal>
-                    <HStack alignItems="center">
-                    <LinkText
-                        size="sm"
-                        fontFamily="$heading"
-                        fontWeight="$semibold"
-                        color="$primary600"
-                        $dark-color="$primary300"
-                        textDecorationLine="none"
-                    >
-                        Read Blog
-                    </LinkText>
-                    {/* <Icon
-                        as={ArrowRightIcon}
-                        size="sm"
-                        color="$primary600"
-                        mt="$0.5"
-                        ml="$0.5"
-                        $dark-color="$primary300"
-                    /> */}
-                    </HStack>
-                </Link>
-                </Card>
-            )}
-            // keyExtractor={(item) => item.id}
-        />
-        )}
-    </>
-);}
-
-const Home = () => {
-  return (
-    <GluestackUIProvider config={config}>
-      {/* <Button>
-        <ButtonText>Hello World</ButtonText>
-      </Button> */}
-
-        <Alert mx="$2.5" action="info" variant="outline">
-        {/* <AlertIcon as={InfoIcon} mr="$3" /> */}
-            <AlertText>
-                We have updated our terms of service. Please review and accept to continue
-                using our service.
-            </AlertText>
-        </Alert>
-        <Berita />
-    </GluestackUIProvider>
-  )
-}
+            </View>
+        </GluestackUIProvider>
+    );
+};
 
 export default Home;
